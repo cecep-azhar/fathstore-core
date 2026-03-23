0d6e87e24eb308c645e02726284b56b21db873b7
Cecep Saeful Azhar Hidayat, ST <122300501+cecep-azhar@users.noreply.github.com>
2026-03-23 16:06:56 +0700

    feat: configure vercel blob storage

--- apps/admin/payload.config.ts
+++ apps/admin/payload.config.ts
@@ -1,4 +1,5 @@
 import { postgresAdapter } from '@payloadcms/db-postgres'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload'
@@ -31,6 +32,12 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+    plugins: [
+          vercelBlobStorage({
+                  collections: { media: true },
+                  token: process.env.BLOB_READ_WRITE_TOKEN,
+          }),
+        ],
   admin: {
     user: 'users',
     importMap: {
